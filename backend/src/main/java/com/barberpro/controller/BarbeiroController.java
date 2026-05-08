ad3d432f54bc7724766054ddb746cf02ecf6ff36
Maycon Mota <mayconmotamendes@gmail.com>
2026-05-08 19:21:30 -0300

    feat: ajustes no confg cloud

--- backend/src/main/java/com/barberpro/controller/BarbeiroController.java
+++ backend/src/main/java/com/barberpro/controller/BarbeiroController.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/barbeiros")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+
 public class BarbeiroController {
 
     private final UsuarioRepository repo;
--- backend/src/main/java/com/barberpro/controller/ServicoController.java
+++ backend/src/main/java/com/barberpro/controller/ServicoController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/servicos")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+
 public class ServicoController {
 
     private final ServicoRepository repo;
